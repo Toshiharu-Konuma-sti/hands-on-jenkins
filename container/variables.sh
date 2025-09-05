df5af2cf6301d807b03381778d7fc55cfa64877e
Toshiharu-Konuma-sti <t-konuma@sios.com>
2025-09-05 23:22:09 +0900

    changed a url to point to the main branch

--- container/variables.sh
+++ container/variables.sh
@@ -4,7 +4,7 @@ ART_PKG_PTN="jfrog-artifactory-oss-*-compose.tar.gz"
 ART_DIR_PTN="artifactory-oss-*"
 
 WEBAPP_PKG_URL="https://github.com/Toshiharu-Konuma-sti/hands-on-webapp-rolling-dice/archive/refs/heads/main.zip"
-# WEBAPP_PKG_URL="https://github.com/Toshiharu-Konuma-sti/hands-on-webapp-rolling-dice/archive/refs/heads/dev202508.zip"
+# WEBAPP_PKG_URL="https://github.com/Toshiharu-Konuma-sti/hands-on-webapp-rolling-dice/archive/refs/heads/dev202509.zip"
 WEBAPP_PROJECTS="webapp-webui webapp-webapi"
 
 JK_HOST_EXT="localhost:8080"
