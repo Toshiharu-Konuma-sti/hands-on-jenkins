dbd44b180c99bdaa6360e2f49510e0c9c69ab726
Toshiharu-Konuma-sti <t-konuma@sios.com>
2026-01-08 22:24:42 +0900

    created a new script

--- setup/step21-gitlab_create_group.sh
+++ setup/step21-gitlab_create_group.sh
@@ -38,4 +38,48 @@ echo ">>> group name = [${GROUP_NAME}]"
 echo ">>> group path = [${GROUP_PATH}]"
 echo ">>> group id = [${GROUP_ID}]"
 
+echo "\n### START: create a group runner"
+
+CMD_RUNNER="curl -v -f -X POST
+	-H \"Authorization: Bearer ${GL_TOKEN}\"
+	-H \"Content-Type: application/json\"
+	-d \"{
+  \\\"runner_type\\\": \\\"group_type\\\",
+  \\\"group_id\\\": ${GROUP_ID},
+  \\\"description\\\": \\\"Group-Runner-API\\\",
+  \\\"tag_list\\\": [],
+  \\\"run_untagged\\\": true,
+  \\\"locked\\\": false,
+  \\\"access_level\\\": \\\"not_protected\\\"
+}\"
+	 \"http://${GITL_HOST}/api/v4/user/runners\""
+
+GL_BODY=$(loop_curl_until_success "${CMD_RUNNER}")
+
+RUNNER_TOKEN=$(echo "${GL_BODY}" | grep -o '"token":"[^"]*"' | sed 's/"token":"//;s/"//')
+
+echo ">>> runner token = [${RUNNER_TOKEN}]"
+
+echo "\n### START: set up the configration for gitlab-runner"
+
+GITLAB_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}} {{end}}' gitlab | awk '{print $NF}')
+echo ">>> gitlab ip = [${GITLAB_IP}]"
+
+docker exec -it gitlab-runner gitlab-runner register \
+  --non-interactive \
+  --url "http://gitlab:13000" \
+  --clone-url "http://gitlab:13000" \
+  --token "${RUNNER_TOKEN}" \
+  --executor "docker" \
+  --docker-image "alpine:latest" \
+  --description "docker-runner-alpine" \
+  --docker-network-mode "host" \
+  --docker-extra-hosts "gitlab:${GITLAB_IP}"
+
+echo ">>> $ docker exec gitlab-runner cat /etc/gitlab-runner/config.toml"
+docker exec gitlab-runner cat /etc/gitlab-runner/config.toml
+
+echo "\n### START: restart gitlab-runner to apply the changed configuration"
+docker restart gitlab-runner
+
 call_show_finish_banner
