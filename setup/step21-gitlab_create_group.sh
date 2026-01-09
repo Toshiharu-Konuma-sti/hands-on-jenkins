4fadde91e077c9e6b3b51825ea32d65169484d56
Toshiharu-Konuma-sti <t-konuma@sios.com>
2026-01-09 10:59:29 +0900

    added an implimentation to register runner

--- setup/step21-gitlab_create_group.sh
+++ setup/step21-gitlab_create_group.sh
@@ -6,15 +6,15 @@ CUR_DIR=$(cd $(dirname $0); pwd)
 
 call_show_start_banner
 
-echo "\n### START: get root's password for GitLab"
+echo "\n### START: get root's password for GitLab ############################"
 
 GL_PASS=$(get_gitlab_root_password)
 
-echo "\n### START: get an access token for GitLab"
+echo "\n### START: get an access token for GitLab ############################"
 
 GL_TOKEN=$(get_gitlab_access_token "${GITL_USER}" "${GL_PASS}" "${GITL_HOST}")
 
-echo "\n### START: create a group"
+echo "\n### START: create a group ############################################"
 
 GROUP_NAME="My Hands-on Group"
 GROUP_PATH="my-hands-on-group"
@@ -38,7 +38,7 @@ echo ">>> group name = [${GROUP_NAME}]"
 echo ">>> group path = [${GROUP_PATH}]"
 echo ">>> group id = [${GROUP_ID}]"
 
-echo "\n### START: create a group runner"
+echo "\n### START: create a group runner #####################################"
 
 CMD_RUNNER="curl -v -f -X POST
 	-H \"Authorization: Bearer ${GL_TOKEN}\"
@@ -60,7 +60,7 @@ RUNNER_TOKEN=$(echo "${GL_BODY}" | grep -o '"token":"[^"]*"' | sed 's/"token":"/
 
 echo ">>> runner token = [${RUNNER_TOKEN}]"
 
-echo "\n### START: set up the configration for gitlab-runner"
+echo "\n### START: set up the configration for gitlab-runner #################"
 
 GITLAB_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}} {{end}}' gitlab | awk '{print $NF}')
 echo ">>> gitlab ip = [${GITLAB_IP}]"
@@ -81,5 +81,30 @@ docker exec gitlab-runner cat /etc/gitlab-runner/config.toml
 
 echo "\n### START: restart gitlab-runner to apply the changed configuration"
 docker restart gitlab-runner
+
+
+for MY_PROJ in ${WEBAPP_PROJECTS}; do
+
+	echo "\n### START: get a project id (repository id) ##########################"
+
+	PROJECT_FULL_PATH="${GITL_USER}/${MY_PROJ}"
+	ENCODED_PATH=$(echo "${PROJECT_FULL_PATH}" | sed 's/\//%2F/g')
+
+	PROJECT_INFO=$(curl -v \
+		-H "Authorization: Bearer ${GL_TOKEN}" \
+		"http://${GITL_HOST}/api/v4/projects/${ENCODED_PATH}")
+
+	PROJECT_ID=$(echo "$PROJECT_INFO" | grep -o '"id":[0-9]*,' | head -n 1 | sed 's/"id"://;s/,//')
+	echo ">>> project name & id = [${MY_PROJ}][${PROJECT_ID}]"
+
+	echo "\n### START: transfer project(repository) from user to group ###########"
+
+	CMD_TRANSFER="curl -v -f -X PUT
+		-H \"Authorization: Bearer ${GL_TOKEN}\"
+		\"http://${GITL_HOST}/api/v4/projects/${PROJECT_ID}/transfer?namespace=${GROUP_ID}\""
+
+	GL_BODY=$(loop_curl_until_success "${CMD_TRANSFER}")
+
+done
 
 call_show_finish_banner
