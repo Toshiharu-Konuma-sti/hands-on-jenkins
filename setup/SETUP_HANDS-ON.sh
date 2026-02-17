d9d805bc3975fe2052c75540e7b49e90a795f6ef
Toshiharu-Konuma-sti <t-konuma@sios.com>
2026-02-18 01:21:15 +0900

    improved the processing to check required commands

--- setup/SETUP_HANDS-ON.sh
+++ setup/SETUP_HANDS-ON.sh
@@ -7,7 +7,8 @@ CUR_DIR=$(cd $(dirname $0); pwd)
 
 start_banner
 
-$CUR_DIR/step01-check_required_command.sh
+check_required_commands "java jq"
+#	$CUR_DIR/step01-check_required_command.sh
 $CUR_DIR/step02-jenkins_create_job.sh
 $CUR_DIR/step03-gitlab_update_admin_setting.sh
 $CUR_DIR/step04-gitlab_import_repository.sh
